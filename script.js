ba27b96e78cde6160073c5625f16ee734c2bfd20
SaraMur <saramurcomercial@gmail.com>
2022-04-13 19:28:31 -0300

    avance js, arreglo de container en css, prueba de eliminacion btn

--- script.js
+++ script.js
@@ -28,7 +28,7 @@ const caracteresSimbolos = document.getElementById('caracteres-simbolos');
 const generatorButton = document.getElementById('generator-button');
 //const copyButton = document.getElementById('copy-button');
 
-//INPUT PASSWORD
+//PASSWORD TEXT
 const passwordText = document.getElementById('password-text');
 
 //INPUT FORM
@@ -37,25 +37,62 @@ const passwordForm = document.getElementById('password-form');
 
 
 //OPTIONS
-const longitud12Valor = longitud12Caracteres.checked;
-const longitud9Valor = longitud9Caracteres.checked;
-const longitud6Valor = longitud6Caracteres.checked;
+// const longitud12Valor = longitud12Caracteres.checked;
+// const longitud9Valor = longitud9Caracteres.checked;
+// const longitud6Valor = longitud6Caracteres.checked;
     
-const reglasSoloLetrasValor = reglasSoloLetras.checked;
-const reglasLecturaSimpleValor = reglasLecturaSimple.checked;
-const reglasTodosCaracteresValor = reglasTodosCaracteres.checked;
+// const reglasSoloLetrasValor = reglasSoloLetras.checked;
+// const reglasLecturaSimpleValor = reglasLecturaSimple.checked;
+// const reglasTodosCaracteresValor = reglasTodosCaracteres.checked;
 
-const caracteresMayusculasValor = caracteresMayusculas.checked;
-const caracteresMinusculasValor = caracteresMinusculas.checked;
-const caracteresSimbolosValor = caracteresSimbolos.checked;
+// const caracteresMayusculasValor = caracteresMayusculas.checked;
+// const caracteresMinusculasValor = caracteresMinusculas.checked;
+// const caracteresSimbolosValor = caracteresSimbolos.checked;
 
 
-const characters = '123456789abcdefghijkmnpqrstuvwxyz!@#$%^&*()ABCDEFGHJKLMNOPQRSTUVWXYZ';
+
+
+//funciones auxiliares
+//LONGITUD AUXILIARES
+
+
+//REGLAS AUXILIARES
+
+
+//CARACTERES AUXILIARES
+// const charactersChosen = () =>{
+
+// }
+
+
+
+const seleccionUsuario = document.getElementsByTagName('input');
+
+const loQueVaUsuario = (seleccionUsuario) =>{
+    return seleccionUsuario.value === checked;
+}
+//¿Porque no recibe los datos de los inputs?
+//¿Como utilizo los datos recibidos de los inputs?
+
+
+
+//!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+//esta la llenaria con los datos obtenidos de reglas y de caracteres
+
+
+
+
+let characters = '';
+
 
 
 //FUNCTIONS********************
-
+//!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+        //esta funcion funciona con la longitud en el parametro y con un array "characters" conformado por lo que el usuario eligió en la seccion de reglas y en la seccion de caracteres. 
 const genPassword = (length) => {
+  
+
     let charactersLength = characters.length;
     let password = '';
     for (let i = 0; i <= length; i++) {
@@ -69,13 +106,6 @@ console.log(genPassword(8));
 console.log(genPassword(5));
 
 
-const genPassword2 = (longitud12Valor, longitud9Valor, longitud6Valor, reglasSoloLetrasValor, reglasLecturaSimpleValor, reglasTodosCaracteresValor, caracteresMayusculasValor, caracteresMinusculasValor, caracteresSimbolosValor) => {
-
-    characters
-
-
-}
-
 
 passwordForm.addEventListener('submit', e =>{
     e.preventDefault()
@@ -83,29 +113,13 @@ passwordForm.addEventListener('submit', e =>{
     
 
 
-    const passwordCreated = genPassword2(longitud12Valor, longitud9Valor, longitud6Valor, reglasSoloLetrasValor, reglasLecturaSimpleValor, reglasTodosCaracteresValor, caracteresMayusculasValor, caracteresMinusculasValor, caracteresSimbolosValor) 
+    // const passwordCreated = genPassword2(longitud12Valor, longitud9Valor, longitud6Valor, reglasSoloLetrasValor, reglasLecturaSimpleValor, reglasTodosCaracteresValor, caracteresMayusculasValor, caracteresMinusculasValor, caracteresSimbolosValor) 
 
     
 });    
 
 
 
-
-
-
-//LONGITUD
-/*const longitud12Caracteres{
-if (longitud12Caracteres.value === checked){
-    return create array. length [11]
-    }
-}
-*/
-
-//REGLAS
-
-
-
-//CARACTERES
 
 
 //BOTONES
