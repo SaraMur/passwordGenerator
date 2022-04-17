58c34dcafd97abd98b923dc308c240786d94965d
SaraMur <saramurcomercial@gmail.com>
2022-04-17 01:34:01 -0300

    ultimos cambios

--- script.js
+++ script.js
@@ -1,32 +1,4 @@
-//HTML ELEMENTS
-//INPUTS DE LONGITUD
-const longitud12Caracteres = document.getElementById('longitud-12-caracteres');
-const longitud9Caracteres = document.getElementById('longitud-9-caracteres');
-const longitud6Caracteres = document.getElementById('longitud-6-caracteres');
 
-
-
-//INPUTS DE REGLAS
-const reglasSoloLetras = document.getElementById('reglas-solo-letras');
-const reglasLecturaSimple = document.getElementById('reglas-lectura-simple');
-const reglasTodosCaracteres = document.getElementById('reglas-todos-caracteres');
-
-
-
-
-
-//INPUTS DE CARACTERES
-const caracteresMayusculas = document.getElementById('caracteres-mayusculas');
-const caracteresMinusculas = document.getElementById('caracteres-minusculas');
-const caracteresNumeros = document.getElementById('caracteres-numeros');
-const caracteresSimbolos = document.getElementById('caracteres-simbolos');
-
-
-
-
-//BUTTONS
-const generatorButton = document.getElementById('generator-button');
-//const copyButton = document.getElementById('copy-button');
 
 //PASSWORD TEXT
 const passwordText = document.getElementById('password-text');
@@ -38,12 +10,8 @@ const passwordForm = document.getElementById('password-form');
 
 
 
-const seleccionUsuario = document.getElementsByTagName('input');
+//const seleccionUsuario = document.getElementsByTagName('input');
 
-const loQueVaUsuario = (seleccionUsuario) =>{
-    return seleccionUsuario.value === checked;
-}
-//¿Porque no recibe los datos de los inputs?
 
 
 
@@ -51,46 +19,82 @@ const loQueVaUsuario = (seleccionUsuario) =>{
 //!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
         //esta funcion funciona con la longitud en el parametro y con un array "characters" conformado por lo que el usuario eligió en la seccion de reglas y en la seccion de caracteres. 
 
-let characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
 
-const genPassword = (characters, length) => {
-  
 
-    let charactersLength = characters.length;
+const longitud12Caracteres = document.getElementById('longitud-12-caracteres');
+const longitud9Caracteres = document.getElementById('longitud-9-caracteres');
+const longitud6Caracteres = document.getElementById('longitud-6-caracteres');
+
+
+const caracteresMayusculas = document.getElementById('caracteres-mayusculas');
+const caracteresMinusculas = document.getElementById('caracteres-minusculas');
+
+const caracteresNumeros = document.getElementById('caracteres-numeros');
+
+const caracteresSimbolos = document.getElementById('caracteres-simbolos');
+
+
+const generatorButton = document.getElementById('generator-button');
+
+
+        
+const genPassword = (letters, length) => {
+            
     let password = '';
+
     for (let i = 0; i <= length; i++) {
-        password += characters.charAt(Math.floor(Math.random() * charactersLength));
+       let random = Math.floor(Math.random() * letters.length);
+        password += letters.charAt(random);
     }
     return password;
 };
 
+
+
+
 const generateMixDatos = () => {
-
-  
-    let length = parseInt(inputLength.value);
-
-    const symbols = ".:,;-+*/¡!<>¿?=()&%$#@°"
+    
+    let letters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    
+ //   let lettersLowerCase = 'abcdefghijklmnopqrstuvwxyz';
+    
+    const symbols = ".:,;-+*/¡!<>¿?=()&%$#@°";
     const numbers = "0123456789";
+    
+    
+    if (longitud12Caracteres.checked)letters.length = 11;
+//    if (longitud9Caracteres.checked) letters.length = 8;
+//    if (longitud6Caracteres.checked) letters.length = 5;
+    
+    let length = letters.length;
+    
+//    if (caracteresMayusculas.checked) lettersLowerCase = lettersLowerCase.toUpperCase;
+//    if (caracteresMinusculas.checked) lettersLowerCase = lettersLowerCase;
 
-    if (caracteresNumeros.checked) characters += numbers;
-    if (caracteresSimbolos.checked) characters += symbols;
 
-    genPassword.innerText /*donde esta innerText?*/ = genPassword (characters, length);
+    if (caracteresNumeros.checked) letters += numbers;
+    if (caracteresSimbolos.checked) letters += symbols;
+
+
+    genPassword.innerText = genPassword (letters, length);
 
 };
 
+generatorButton.addEventListener('click', ()=>{
+    generateMixDatos();
+});
 
 
 
 
-passwordForm.addEventListener('submit', e =>{
-    e.preventDefault()
+// const input= document.getElementsByTagName(input)
 
+// input.addEventListener('change', (e) =>{
+//     console.log(elem);
+
+//     genPassword(e.target.value);
     
-    const genPassword = (characters, length);
-
-    
-});    
+// });    
 
 
 
